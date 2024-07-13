77645089f20526bc8b38bb3d92e08b6588d1a6b0
ramandeep kaur <ramank6238299@gmail.com>
2024-07-13 19:20:45 +0530

    blog fix

--- src/components/Cards/BlogCard/BlogCard.tsx
+++ src/components/Cards/BlogCard/BlogCard.tsx
@@ -1,12 +1,11 @@
 import Link from "next/link";
-import Image from "next/image";
 
 export const BlogCard = ({ cardInfo }: any) => {
   const { title, link, thumbnail } = cardInfo;
   return (
     <Link href={link}>
       <div className="w-[252px] bg-[#FBF8F6]">
-        <Image src={thumbnail} alt="" className="rounded-md mb-3" height={150} />
+        <img src={thumbnail} alt="" className="rounded-md mb-3" height={"150px"} width={"150px"} />
         <p className=" text-start  text-base font-semibold">
           {title.length > 50 ? `${title.slice(0, 50)}...` : title}{" "}
         </p>
