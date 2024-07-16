e5ba5b098e262f45eb961d2656cca68459d23dca
ramandeep kaur <ramank6238299@gmail.com>
2024-07-16 18:48:08 +0530

    dployment err

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,4 @@
 import "@/styles/globals.css";
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import '../styles/globals.css'; 
 import type { AppProps } from "next/app";
 import {
   QueryClient,
